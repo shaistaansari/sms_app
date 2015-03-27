d44e36098c14829cfdc9c02dfcc99ce48d9ad5b8
shaistaansari <shaansari@hotmail.com>
2015-03-27 00:21:15 +0000

    added message

--- Gemfile
+++ Gemfile
@@ -12,6 +12,8 @@ group :production do
     gem 'rails_12factor'
 end
 
+gem 'twilio-ruby'
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '4.0.3'
 # Use Uglifier as compressor for JavaScript assets
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,17 +5,26 @@ class UsersController < ApplicationController
   end
  
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
+    
     if @user.save
       render text: "Thank you! You will receive an SMS shortly with verification instructions."
-    else
+        # Instantiate a Twilio client
+         client = Twilio::REST::Client.new(TWILIO_CONFIG['sid'], TWILIO_CONFIG['token'])
+      
+         # Create and send an SMS message
+        client.account.sms.messages.create(
+        from: TWILIO_CONFIG['from'],
+        to: @user.phone,
+        body: "Thanks for signing up. To verify your account, please reply HELLO to this message."
+      )
+      else
       render :new
     end
   end
 end
   private
-  
-  def User_params
+  def user_params
     params.require(:user).permit(:email, :name, :phone)
   end
 
